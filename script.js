bf9664d213c494d420b2c772d5725e3383ec4417
Bea <b.caballero-martin@imperial.ac.uk>
2025-02-11 15:31:15 +0000

    Created CMALT portfolio with a modern, minimalist design

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const galleryImages = document.querySelectorAll(".gallery-img");
-
-    galleryImages.forEach(img => {
-        img.addEventListener("click", function () {
-            const modal = document.createElement("div");
-            modal.classList.add("modal");
-            modal.innerHTML = `
-                <div class="modal-content">
-                    <span class="close">&times;</span>
-                    <img src="${img.src}" alt="${img.alt}">
-                </div>
-            `;
-            document.body.appendChild(modal);
-
-            // Close Modal
-            modal.querySelector(".close").addEventListener("click", function () {
-                modal.remove();
-            });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
         });
     });
 });
